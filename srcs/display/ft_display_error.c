8593c3d3177de941802109600920bb80ed47c0a8
lburnet <75073111+aleyra@users.noreply.github.com>
2021-03-08 15:42:55 +0100

    Add files via upload

--- srcs/display/ft_display_error.c
+++ srcs/display/ft_display_error.c
@@ -6,11 +6,12 @@
 /*   By: lburnet <lburnet@student.42lyon.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/03 11:27:35 by lburnet           #+#    #+#             */
-/*   Updated: 2021/03/03 13:15:53 by lburnet          ###   ########lyon.fr   */
+/*   Updated: 2021/03/08 15:01:50 by lburnet          ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minirt.h"
+#include "errors.h"
 
 static void	print_error_type_message_n1(int cas)
 {
@@ -22,11 +23,25 @@ static void	print_error_type_message_n1(int cas)
 		printf("The diameter or the heigh of the cylinder is not positive\n");
 	else if (cas == 10)
 		printf("All 3 points of the triangle are aligned\n");
+	else if (cas == 11)
+	{
+		printf("Program launch without any parameter\n");
+		printf("please add a .rt file as an parameter\n");
+	}
+	else if (cas == 12)
+	{
+		printf("Too many parameters\n");
+		printf("Needs max 2 parameters : a .rt file and \"--save\"\n");
+	}
 }
 
-static void	print_error_type_message(int cas)
+static void	print_error_type_message(t_error cas)
 {
-	if (cas == 0)
+
+	if (cas != ERROR_OK)
+		printf("%s\n", g_error_msg[cas]);
+	//
+	if (cas == ERROR_FILE_NOT_RT)
 		printf("The file is not a .rt\n");
 	else if (cas == 1)
 		printf("The 2nd parameter is not \"--save\"\n");
@@ -50,7 +65,7 @@ static void	print_error_type_message(int cas)
 		print_error_type_message_n1(cas);
 }
 
-int			ft_display_error(int cas)
+int	ft_display_error(int cas)
 {
 	printf("Error\n");
 	print_error_type_message(cas);
