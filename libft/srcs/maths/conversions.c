70898b44d8744b89c78da71f2fd3a5c96170546c
lburnet <75073111+aleyra@users.noreply.github.com>
2021-03-17 15:25:38 +0100

    wed 210317

---
+++ libft/srcs/maths/conversions.c
@@ -0,0 +1,50 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   conversions.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lburnet <lburnet@student.42lyon.fr>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/03/10 10:49:07 by lburnet           #+#    #+#             */
+/*   Updated: 2021/03/17 10:31:35 by lburnet          ###   ########lyon.fr   */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "maths3d.h"
+
+float	deg_to_rad(int d)
+{
+	float	r;
+
+	d = d % 360.0f;
+	r = d * M_PI / 180.0f;
+	return (r);
+}
+
+int	rad_to_deg(float r)
+{
+	float	d;
+
+	while (r > 2.0f * M_PI)
+		r -= 2.0f * M_PI;
+	while (r < -2.0f * M_PI)
+		r += 2.0f * M_PI;
+	d = 180.0f * r / M_PI;
+	return (round(d));
+}
+
+float	angle_one_polaris(t_vec3 v)
+{
+	float	r;
+
+	r = acos(v.z * Q_rsqrt(norme_vec3_power2(v)));
+	return (r);
+}
+
+float	angle_two_polaris(t_vec3 v)
+{
+	float	r;
+
+	r = atan(v.y * Q_rsqrt(x * x));
+	return (r);
+}
---
+++ libft/srcs/maths/some_calc3d.c
@@ -0,0 +1,71 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   some_calc3d.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lburnet <lburnet@student.42lyon.fr>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/03/10 12:19:36 by lburnet           #+#    #+#             */
+/*   Updated: 2021/03/11 13:27:39 by lburnet          ###   ########lyon.fr   */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "maths3d.h"
+
+float	norme_vec3_power2(t_vec3 a)
+{
+	float	n;
+
+	n = a.x * a.x + a.y * a.y + a.z * a.z;
+	return (n);
+}
+
+t_vec3	vec3_from_2pts(t_vec3 a, t_vec3 b)
+{
+	t_vec3	ab;
+
+	ab.x = b.x - a.x;
+	ab.y = b.y - a.y;
+	ab.z = b.z - a.z;
+	return (ab);
+}
+
+t_vec3	sum_alg_2vec3(float l, t_vec3 v, float m, t_vec3 u)
+{
+	t_vec3	lvpmu;
+
+	lvpmu.x = l * v.x + m * u.x;
+	lvpmu.y = l * v.y + m * u.y;
+	lvpmu.z = l * v.z + m * u.z;
+	return (lvpmu);
+}
+
+float	angle_vec3v_vec3u(t_vec3 v, t_vec3 u)
+{
+	float	rad;
+
+	rad = acos((norme_vec3_power2(sum_alg_2vec3(
+						1, v, 1, u)) - norme_vec3_power2(
+					v) - norme_vec3_power2(u)) * (0.5 * Q_rsqrt(
+					norme_vec3_power2(v)) * Q_rsqrt(norme_vec3_power2(u))));
+	return (rad);
+}
+
+int	in_angular_sector(t_vec3 a, t_vec3 b, t_vec3 c, t_vec3 p)
+{
+	float	abac;
+	float	abap;
+	t_vec3	ab;
+	t_vec3	ac;
+	t_vec3	ap;
+
+	ab = vec3_from_2pts(a, b);
+	ac = vec3_from_2pts(a, c);
+	ap = vec3_from_2pts(a, p);
+	abac = angle_vec3v_vec3u(ab, ac);
+	abap = angle_vec3v_vec3u(ab, ap);
+	if (abac >= abap)
+		return (1);
+	else
+		return (-1);
+}
